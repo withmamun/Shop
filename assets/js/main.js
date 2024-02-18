88c34cc97ee3aa3194ced0447faf118677455a7a
Md Mamun Mia <mamunahammedco59@gmail.com>
2024-02-19 02:12:41 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+    "use strict";
+    jQuery(document).ready(function ($) {
+
+        $( ".product__fav__red" ).on( "click", function() {
+            $( this ).toggleClass( "active" );
+          });
+
+
+
+
+    }); //---document-ready-----
+
+}(jQuery));
